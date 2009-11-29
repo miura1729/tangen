c3e750e649b2f55a5fda97f08237a257db247444
U-KOZUE\kozue <m-72@tf6.so-net.ne.jp>
2009-11-29 22:13:24 +0900

    Very basic test

--- tangen.rb
+++ tangen.rb
@@ -1,4 +1,93 @@
-class Word
-  def initalize
+# -*- coding: cp932 -*-
+
+class KlassWListTable
+  def initialize
+    @table = []
+    (1..7).each do |n|
+      @table[n] = Array.new
+    end
+  end
+
+  def add(word, n)
+    @table[n].push word
+  end
+
+  def get_word(n)
+    wlist = @table[n]
+    lsiz = wlist.size
+    wlist[rand(lsiz)]
   end
 end
+
+class Word
+  def initiailize(word, klass)
+    @word = word
+    @klass = klass
+  end
+
+  def get_word(n)
+    @word
+  end
+end
+
+Klasses = {
+  :Noum => KlassWListTable.new,
+  :Verb => KlassWListTable.new,
+  :Adj => KlassWListTable.new,
+  :Postp => KlassWListTable.new,
+}
+
+class TranslateTable
+  def initalize(word)
+    @word = word
+    @table = []
+    @total = 0
+  end
+
+  def add(w_or_tab, weight = 1) 
+    @total += weight
+    @table.push [w_or_tab, weight]
+  end
+
+  def get_next_word(n)
+    wc = 0
+    @table.each do |wot, weight|
+      wc += weight
+      if rand(@total) < wc then
+        return wot.get_word(n)
+      end
+    end
+    raise "Internal error maybe bugs"
+  end
+end
+
+WORD_TABLE = [
+  ["ŽR", 2, :Noum],
+  ["ì", 2, :Noum],
+  ["—ñŽÔ", 3, :Noum],
+  ["Ž©“®ŽÔ", 4, :Noum],
+  ["’j", 3, :Noum],
+  ["—", 3, :Noum],
+  ["”L", 2, :Noum],
+  ["Œ¢", 2, :Noum],
+
+  ["‘–‚é", 3, :Verb],
+  ["•à‚­", 3, :Verb],
+  ["”R‚¦‚é", 3, :Verb],
+  ["—¬‚ê‚é", 4, :Verb],
+
+  ["”’‚¢", 3, :Adj],
+  ["Ô‚¢", 3, :Adj],
+  ["”ü‚µ‚¢", 4, :Adj],
+
+  ["‚Í", 1, :Postp],
+  ["‚ª", 1, :Postp],
+  ["‚Ì", 1, :Postp],
+  ["‚Æ", 1, :Postp],
+]
+
+WORD_TABLE.each do |word, size, klass|
+  Klasses[klass].add(word, size)
+end
+
+p Klasses[:Noum].get_word(3)
